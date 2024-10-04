0b4f1e929c8c7fb1b44e6854e0f8e8cf58968f8b
Pulavamsi2001 <pulavamsi2001@gmail.com>
2024-10-04 17:21:08 +0530

    new

--- src/Pages/Appointment.jsx
+++ src/Pages/Appointment.jsx
@@ -27,13 +27,14 @@ const Appointment = () => {
             <img className='bg-primary w-full sm:max-w-72 rounded-lg' src={docInfo.image} alt="" />
          </div>
          <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0'>
-          <p className='flex gap-2 font-medium text-2xl'>{docInfo.name} <img className='w-5' src={assets.verified_icon} alt="" /></p>
+          <p className='flex gap-2 font-semibold text-2xl'>{docInfo.name} <img className='w-5' src={assets.verified_icon} alt="" /></p>
           <div>
             <p>{docInfo.degree} - {docInfo.speciality}</p>
           </div>
+          <br />
           <div>
-            <p className='flex gap-1'>About <img className='w-3' src={assets.info_icon} alt="" /> </p>
-            <p>{docInfo.about}</p>
+            <p className='flex gap-1 font-medium'>About <img className='w-3' src={assets.info_icon} alt="" /> </p>
+            <p className='text-gray-700'>{docInfo.about}</p>
           </div>
 
 
