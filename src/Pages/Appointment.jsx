11b6a8ab8e2ea0491b7f1fbab84d1e6a42260ac4
Pulavamsi2001 <pulavamsi2001@gmail.com>
2024-10-04 17:40:10 +0530

    neww

--- src/Pages/Appointment.jsx
+++ src/Pages/Appointment.jsx
@@ -2,12 +2,13 @@ import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { AppContext } from '../Context/AppContext';
 import { assets } from '../assets/assets';
-import About from './About';
+
 
 const Appointment = () => {
   const { docId } = useParams();
   const { doctors } = useContext(AppContext);
   const [docInfo, setDocInfo] = useState(null);
+
 
   const fetchDocInfo = () => {
     const docInfo = doctors.find(doc => doc._id === docId);
@@ -36,10 +37,17 @@ const Appointment = () => {
             <p className='flex gap-1 font-medium'>About <img className='w-3' src={assets.info_icon} alt="" /> </p>
             <p className='text-gray-700'>{docInfo.about}</p>
           </div>
-
-
+          <p className='text-gray-700 font-medium'>Appointment fee: <span className='text-black'>₹{docInfo.fees}</span></p>
          </div>
       </div>
+
+
+
+
+
+
+
+      <div className=' my-10 text-center text-2xl font-medium text-gray-600 '> <p>----------- Booking Slots -----------</p> <br /> <h1>Coming Soon</h1></div>
 
     </div>
   );
